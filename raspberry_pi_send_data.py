c5724c5fbd4d2cca34aad49193200db5b00e0eb9
MarShin <martinshin95@gmail.com>
2018-03-29 04:17:56 +0800

    modify script to run on rpi zero w

--- raspberry_pi_send_data.py
+++ raspberry_pi_send_data.py
@@ -2,9 +2,10 @@ import requests
 import pprint
 import json
 import subprocess
+import time
 
 endpoint = 'http://35.229.175.184/predict'
-filename = './waves.mp3'
+filename = './rain.mp3'
 
 def send_raw(wav):
     files = {'file': open(wav,'rb')}
@@ -14,17 +15,27 @@ def send_raw(wav):
     print('Status: '+str(r))
     # print(r.content)
     pprint.pprint(json.loads(r.content))
-    print('Time elapsed: ' + str(r.elapsed))
+    print('API Time elapsed: ' + str(r.elapsed))
 
 
-send_raw(filename)
+#send_raw(filename)
 
-# while (True):
-#     p = subprocess.run(["arecord", "sample.wav", "-d", "5"], input=stdin, stdout=subprocess.PIPE, universal_newlines=True)
-#     out = p.stdout.strip()
-#     err = p.stderr
-#     if stdin == out:
-#         print('OK')
-#     else:
-#         print('failed: ' + out)
-#         print('error: ' + err)
+while (True):
+    start_time = time.time()
+    #p = subprocess.run(["arecord", "sample.wav", "-d", "5"], input=stdin, stdout=subprocess.PIPE, universal_newlines=True)
+    p = subprocess.run(["arecord", "sample.wav", "-d", "5"])
+    #out = p.stdout.strip()
+    #err = p.stderr
+    #if stdin == out:
+    #    print('OK')
+    #else:
+    #    print('failed: ' + out)
+    #    print('error: ' + err)
+    print('subprocess: ')
+    print(p)
+
+    send_raw(filename)
+    print("Total time: %s seconds"%(time.time()-start_time))
+    print('sleep for 5 seconds')
+    time.sleep(5)
+    print()
